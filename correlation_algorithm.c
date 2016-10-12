d604f01122f5c71f283a0057d321bf421ccde810
23Pstars <23Pstars@gmail.com>
2016-10-12 12:01:46 +0700

    update SAD, ZSAD, SSD, ZSSD

--- correlation_algorithm.c
+++ correlation_algorithm.c
@@ -7,44 +7,145 @@
 
 #include "correlation_algorithm.h"
 
-/* Sum of Absolute Differences */
-u_int16_t SAD(u_int8_t *source, u_int8_t *target, u_int16_t height, u_int16_t width, int8_t h_shift, int8_t w_shift) {
+/**
+ * Sum of Absolute Differences
+ *
+ * @param source, block yang akan dicari korelasinya
+ * @param target, block template yang menjadi pembanding, tinggi max adalah shift (-) dan (+) dari tinggi source
+ * @param height, tinggi block dari source
+ * @param width, lebar block dari source atau target
+ * @param h_shift, geser vertical
+ * @param w_shift, geser horizontal
+ * @return
+ */
+u_int32_t SAD(u_int8_t *source, u_int8_t *target, u_int16_t height, u_int16_t width, int8_t h_shift, int8_t w_shift) {
 
-    u_int8_t i, j;
-    u_int16_t sum = 0;
+    int16_t i, j;
+    u_int32_t sum = 0;
 
-    for (i = 0; i < height; i++)
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
 
-        for (j = 0; j < width; j++)
+            /**
+             * jika ada over-index pada horizontal
+             * set nilai = 0
+             */
+//            if (i + h_shift < 0 || i + h_shift > height - 1 || j + w_shift < 0 || j + w_shift > width - 1)
+//                sum += target[(i * width) + j];
+//                sum += 0;
+//            else
+//                sum += abs(source[((i + h_shift) * width) + (j + w_shift)] - target[(i * width) + j]);
 
-            sum += abs((i + h_shift > height - 1 || j + w_shift > width - 1 ? 0 : source[
-                    ((i + h_shift) * width) + j + w_shift]) - target[(i * width) + j]);
+            /**
+             * versi 2
+             * shift vertical tetap mengambil dari memory sebelumnya,
+             * jadi yang dihitung hanya shift horizontal saja
+             */
+            if (j + w_shift < 0 || j + w_shift > width - 1)
+//                sum += 0;
+                sum += source[(i * width) + j];
+            else
+                sum += abs(source[(i * width) + j] - target[((i + h_shift) * width) + (j + w_shift)]);
+
+        }
+    }
 
     return sum;
 }
 
-/* Zero-mean Sum of Absolute Differences */
+/**
+ * Sum of Squared Differences
+ *
+ * @param source
+ * @param target
+ * @param height
+ * @param width
+ * @param h_shift
+ * @param w_shift
+ * @return
+ */
+u_int32_t SSD(u_int8_t *source, u_int8_t *target, u_int16_t height, u_int16_t width, int8_t h_shift, int8_t w_shift) {
+
+    int16_t i, j;
+    u_int32_t sum = 0;
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+
+            if (j + w_shift < 0 || j + w_shift > width - 1)
+                sum += 0;
+//                sum += abs((source[(i * width) + j]) << 2);
+            else
+                sum += abs((source[(i * width) + j] - target[((i + h_shift) * width) + (j + w_shift)]) << 2);
+
+        }
+    }
+
+    return sum;
+}
+
+/**
+ * Zero-mean Sum of Absolute Difference
+ *
+ * @param source
+ * @param target
+ * @param height
+ * @param width
+ * @param h_shift
+ * @param w_shift
+ * @return
+ */
 float ZSAD(u_int8_t *source, u_int8_t *target, u_int16_t height, u_int16_t width, int8_t h_shift, int8_t w_shift) {
 
     u_int8_t i, j;
-    float sum = 0, source_mean = mean(source, height * width), target_mean = mean(target, height * width);
+    u_int16_t length = height * width;
+    float sum = 0, source_mean = mean(source, length, 0), target_mean = mean(target, length, h_shift * width);
 
-    for (i = 0; i < height; i++)
+    for (i = 0; i < height; i++) {
 
-        for (j = 0; j < width; j++)
+        for (j = 0; j < width; j++) {
 
-            sum += fabs(((i + h_shift > height - 1 || j + w_shift > width - 1 ? 0 : source[((i + h_shift) * width) + j +
-                                                                                           w_shift]) - source_mean) -
-                        (target[(i * width) + j] - target_mean));
+            if (j + w_shift < 0 || j + w_shift > width - 1)
+//                sum += 0;
+                sum += source[(i * width) + j];
+            else
+                sum += fabs(source[(i * width) + j] - source_mean - target[((i + h_shift) * width) + (j + w_shift)] +
+                            target_mean);
+
+        }
+    }
+
+    return sum;
+}
+
+float ZSSD(u_int8_t *source, u_int8_t *target, u_int16_t height, u_int16_t width, int8_t h_shift, int8_t w_shift) {
+
+    u_int8_t i, j;
+    u_int16_t length = height * width;
+    float sum = 0, source_mean = mean(source, length, 0), target_mean = mean(target, length, h_shift * width);
+
+    for (i = 0; i < height; i++) {
+
+        for (j = 0; j < width; j++) {
+
+            if (j + w_shift < 0 || j + w_shift > width - 1)
+//                sum += 0;
+                sum += powf(source[(i * width) + j], 2);
+            else
+                sum += powf((source[(i * width) + j] - source_mean - target[((i + h_shift) * width) + (j + w_shift)] +
+                             target_mean), 2);
+
+        }
+    }
 
     return sum;
 }
 
 /* Mean of Block array */
-float mean(u_int8_t *block, u_int16_t length) {
-    u_int8_t i;
+float mean(u_int8_t *block, u_int16_t length, u_int16_t shift) {
+    u_int16_t i;
     float sum = 0;
     for (i = 0; i < length; i++)
-        sum += block[i];
+        sum += *(block + i + shift);
     return sum / length;
 }
--- correlation_algorithm.h
+++ correlation_algorithm.h
@@ -2,8 +2,12 @@
 // Created by Ahmad Zafrullah on 10/3/16.
 //
 
-u_int16_t SAD(u_int8_t *, u_int8_t *, u_int16_t, u_int16_t, int8_t, int8_t);
+u_int32_t SAD(u_int8_t *, u_int8_t *, u_int16_t, u_int16_t, int8_t, int8_t);
+
+u_int32_t SSD(u_int8_t *, u_int8_t *, u_int16_t, u_int16_t, int8_t, int8_t);
 
 float ZSAD(u_int8_t *, u_int8_t *, u_int16_t, u_int16_t, int8_t, int8_t);
 
-float mean(u_int8_t *, u_int16_t);
+float ZSSD(u_int8_t *, u_int8_t *, u_int16_t, u_int16_t, int8_t, int8_t);
+
+float mean(u_int8_t *, u_int16_t, uint16_t);
